b4aceaa5cfba117fa714c1d43b79fbddfaea43cc
dmcneill15 <dvveijeren@gmail.com>
2024-07-20 11:18:50 +1200

    class_exercise9 Error boundary

---
+++ class_exercise9/src/ErrorMessage.jsx
@@ -0,0 +1,14 @@
+
+function ErrorMessage({error, resetErrorBoundary}){
+    console.error(error);
+
+    return(
+        <div className="ErrorMessage">
+            <p>An error occured:</p>
+            <pre>{error.message}</pre>
+            <button onClick={()=>resetErrorBoundary()}>Try Again?</button>
+        </div>
+    );
+}
+
+export default ErrorMessage;
--- class_exercise9/src/main.jsx
+++ class_exercise9/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ErrorBoundary FallbackComponent={ErrorMessage}>
-    <App /> {/* can wrap App or other high-level parent components */}
-  </ErrorBoundary>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
