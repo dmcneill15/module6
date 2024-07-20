0e78fb03b62e1fae28566ed881f11fbea08776bb
dmcneill15 <dvveijeren@gmail.com>
2024-07-20 14:05:57 +1200

    Updated class exercise 6 - corrected conversion function

--- class_exercise6/src/Weather.jsx
+++ class_exercise6/src/Weather.jsx
@@ -3,12 +3,18 @@ import { useState } from "react";
 // Parent component storing common state data
 function Weather() { // copy to Weather.jsx
     // two separate state values to store weather data
-    const [weather, setWeather] = useState('sunny')
-    const [tempCelcius, setTempCelcius] = useState(27)
+    const [weather, setWeather] = useState('sunny');
+    //const [tempCelcius, setTempCelcius] = useState(27);
+
+    const[temp, setTemp] = useState(27);
+    const[units, setUnits] = useState('C');
+
     // handler function to update both state values at once
-    const handleWeatherChange = (newWeather, newTemp) => {
-        setWeather(newWeather)
-        setTempCelcius(newTemp)
+    const handleWeatherChange = (newWeather, newTemp, units) => {
+        setWeather(newWeather);
+        //setTempCelcius(newTemp)
+        setTemp(newTemp);
+        setUnits(units);
     }
 
     return (
@@ -17,29 +23,44 @@ function Weather() { // copy to Weather.jsx
             <div>
                 <strong>{weather}</strong> with a temp of
                 {/* Child component to display temp - needs temp value as prop */}
-                <Temperature temp={tempCelcius} units="C" />
+                {/*<Temperature temp={tempCelcius} units="C" />*/}
+                <Temperature temp={temp} units={units} />
             </div>
             {/* Child component to update the weather - needs handler function as prop */}
-            <CheckWeather onWeatherChange={handleWeatherChange} />
-            <ConvertTemp onTempConvert={handleWeatherChange} />
+            <CheckWeather onWeatherChange={handleWeatherChange} units={units} />
+            <ConvertTemp onTempConvert={handleWeatherChange} weather={weather} temp={temp} units={units}/>
         </div>
     )
 }
 export default Weather;
 
+// Child component to display and convert temp if needed
+function Temperature({ temp, units = 'C' }) { // default to celcius
+    // convert to Fahrenheit if units is F (or not C)
+    //let displayTemp = units === 'C' ? temp : (temp * 9 / 5) + 32
+
+    return (
+        <span className="Temperature">
+            <strong> {parseInt(temp)}&deg;{units} </strong>
+        </span>
+    )
+}
+// ++ Try adding a button to convert between C and F temps
 
 // Child component - receives parent state handler via props
 // child component is controlled - does not contain any states of their own
-function CheckWeather(props) {
+function CheckWeather({onWeatherChange, units}) {
     const weatherTypes = ['sunny', 'windy', 'raining', 'cloudy', 'storm', 'heatwave'];
     // generates new random weather data and updates state via prop
     const randomWeather = () => {
         let newTemp = Math.floor(Math.random() * 40);
         let newWeatherIndex = Math.floor(
             Math.random() * weatherTypes.length);
+            
         // ++ try to destructure this function from the props object
+        //props.onWeatherChange(weatherTypes[newWeatherIndex], newTemp, units); -> worked when only sending props:CheckWeather(props). Updated to include units for temp conversion funciton
         //.onWeatherChange is the equivalent of handleWeatherChange in the parent
-        props.onWeatherChange(weatherTypes[newWeatherIndex], newTemp)
+        onWeatherChange(weatherTypes[newWeatherIndex], newTemp, units);
     }
     return (
         <button onClick={randomWeather}>Check Weather</button>
@@ -47,40 +68,25 @@ function CheckWeather(props) {
 }
 // ++ Add some more weather types of your own
 
-
-// Child component to display and convert temp if needed
-function Temperature({ temp, units = 'C' }) { // default to celcius
-    // convert to Fahrenheit if units is F (or not C)
-    let displayTemp = units === 'C' ? temp : (temp * 9 / 5) + 32
-
-    return (
-        <span className="Temperature">
-            <strong> {parseInt(displayTemp)}&deg;{units} </strong>
-            
-        </span>
-    )
-}
-// ++ Try adding a button to convert between C and F temps
-
-function ConvertTemp(props){
-
+function ConvertTemp({onTempConvert, weather, temp, units}){
 
     const convertTemp = () => {
-        if(units=='C'){
-            displayTemp = (temp * 9 / 5) + 32;
-            units = 'F';
-        }
-        else if(units == 'F')
-        {
-            displayTemp = (temp-32) * 5/9;
-        }
+        let newTemp = temp;
+        let newUnits = units;
 
-        props.onTempConvert(props.weather, )
+        if(newUnits=='C'){
+            newTemp = (newTemp * 9 / 5) + 32;
+            newUnits = 'F';
+        }
+        else if(newUnits == 'F')
+        {
+            newTemp = (newTemp-32) * 5/9;
+            newUnits = 'C';
+        }
+        onTempConvert(weather, newTemp, newUnits);
     }
 
     return(
         <button onClick={convertTemp}>Convert</button>
     )
 }
-
-//come back to this one - will need to use the Temperature function to display the new temp - not necessarily the handleWeather Change function
